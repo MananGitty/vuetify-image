f771c7cd7e10a2c3be7aec5425b5e5d665cb9178
MananGitty <abdulmananmustafa@gmail.com>
2023-01-23 19:48:45 +0500

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<v-app>
+  {{ isWithColor }}
+<SideBar @selectOption="onSelectOption"/>
+
+<v-main> 
+   
+  <ImageGallery :isWithColor="isWithColor"/>
+
+</v-main>
+</v-app>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import ImageGallery from './components/ImageGallery.vue';
+import SideBar from './components/SideBar.vue';
+import { ref } from '@vue/reactivity'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    const isWithColor =ref(true)
+    const onSelectOption = (isColor)=>{
+      isWithColor.value = isColor
+    }
+
+
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,43 @@
+<template>
+    <v-navigation-drawer 
+    expand-on-hover
+    rail>
+    <v-list>
+          <v-list-item
+            prepend-avatar="https://randomuser.me/api/portraits/men/85.jpg"
+            title="Abdul Manan"
+            subtitle="abdulmanan@gmail.com"
+          ></v-list-item>
+        </v-list>
+
+        <v-divider/>
+
+            <v-list density="compact" nav>
+                <v-list-item
+                prepend-icon="mdi-image"
+                title="Color Gallery"
+                value="color"
+                @click="selectOption(true)"
+                />
+                <v-list-item
+                prepend-icon="mdi-dialpad"
+                title="Grey Scale"
+                value="color"
+                @click="selectOption(false)"
+                />
+            </v-list>
+
+    </v-navigation-drawer>
+</template>
+
+<script setup>
+import { defineEmits } from 'vue';
+
+    const emits = defineEmits (['selectOption',])
+     const selectOption =(isColor) =>{
+        emits('selectOption', isColor)
+    }
+
+
+
+</script>
